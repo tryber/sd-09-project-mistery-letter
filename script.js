c780e844a0640521a11ada9b07f90a7b2077e5cc
johnealves <johne_af@hotmail.com>
2020-12-15 12:36:46 -0300

    add more class

--- script.js
+++ script.js
@@ -6,22 +6,18 @@ const optionClass = {
   key2: ['medium', 'big', 'reallybig'],
   key3: ['rotateleft', 'rotateright'],
   key4: ['skewleft', 'skewright'],
-}
+};
 
 function ramdonClass() {
-  const g1 = ['newspaper', 'magazine1', 'magazine2'];
-  const g2 = ['medium', 'big', 'reallybig'];
-  const g3 = ['rotateleft', 'rotateright'];
-  const g4 = ['skewleft', 'skewright'];
   const firstNumber = Math.ceil(Math.random() * 4);
   const chave1 = optionClass[`key${firstNumber}`];
-  const class1 = chave1[Math.floor(Math.random() * chave1.length)]
+  const class1 = chave1[Math.floor(Math.random() * chave1.length)];
   let secondNumber = Math.ceil(Math.random() * 4);
   if (secondNumber === firstNumber) {
     secondNumber = Math.ceil(Math.random() * 4);
   }
   const chave2 = optionClass[`key${secondNumber}`];
-  const class2 = chave2[Math.floor(Math.random() * chave2.length)]
+  const class2 = chave2[Math.floor(Math.random() * chave2.length)];
 
   return `${class1} ${class2}`;
 }

0f370c571fcf7981cb245436b4acb2ceee04c550
johnealves <johne_af@hotmail.com>
2020-12-15 10:50:16 -0300

    change format text letter

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ function generateletter() {
   buttonLetter.addEventListener('click', function () {
     if (inputText.value === '' || !inputText.value.trim()) {
       const span = document.createElement('span');
-      span.innerHTML = 'Por favor, digite o conteúdo da carta';
+      span.innerHTML = 'Por favor, digite o conteúdo da carta.';
       letter.appendChild(span);
     } else {
       const text = inputText.value;
       const textSplit = text.split(' ');
       for (let index = 0; index < textSplit.length; index += 1) {
         const span = document.createElement('span');
-        span.innerHTML = `${textSplit[index]} `;
+        span.innerHTML = textSplit[index];
+        span.innerHTML += " ";
         letter.appendChild(span);
       }
     }

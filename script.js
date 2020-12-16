9ecc59b9f50334dc147a370db8eabc7a04624815
MZTBTG <mztbtg@hotmail.com>
2020-12-16 20:31:17 -0300

    corrigindo botão que adiciona textos

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const textInput = document.getElementById('carta-texto');
 const textResult = document.getElementById('carta-gerada');
 
 function addText() {
-  let word = textInput.value.split(' ');
+  const word = textInput.value.split(' ');
   for (let index in word) {
     const newWord = document.createElement('span');
     newWord.className = 'teste';
-    newWord.innerText += `${word[index]} `;
+    newWord.innerText += `${word[index]}`;
     textResult.appendChild(newWord);
   }
 }
 
 window.onload = function init() {
   buttonCreateText.addEventListener('click', addText);
-}
+};

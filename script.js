08580ab366dc659350d5a1af45e755b63bae4a66
Qu4k3r <neves0007@gmail.com>
2020-12-16 10:03:12 -0300

    Requisitos 3 & 4

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Requisito 3 & 4
+- apertar no botao para gerar a carta
+- o texto deve permanecer no campo input
+- cada palavra deve ser colocada em uma tag span
+- toda tag span deve ser adicionada como filha da tag p
+*/
+
+function createSpan(text) {
+  const mySpan = document.createElement('span');
+  mySpan.innerText = text;
+  return mySpan;
+}
+
+function addSpanToParagraph() {
+  const generateButton = document.querySelector('button');
+  const myParagraph = document.querySelector('p');
+  generateButton.addEventListener('click', () => {
+    const wordsList = document.querySelector('input').value.split(' ');
+    wordsList.forEach((word) => {
+      const newSpan = createSpan(word);
+      myParagraph.appendChild(newSpan);
+    });
+  })
+}
+
+addSpanToParagraph();

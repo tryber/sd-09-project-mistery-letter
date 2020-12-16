dd5944b5b9b6e8123941a944f2d26deb39175796
Qu4k3r <neves0007@gmail.com>
2020-12-16 12:01:30 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* Requisito 5 - Se o usuário não preencher o campo ou preencher com apenas espaços vazios adicionar a mensagem 'Por favor, digite o conteúdo da carta. */
+
+function verifyInput() {
+  if (document.querySelector('input').value.trim() === '') {
+    return alert('Por favor, digite o conteúdo da carta.')
+  }
+}
+
 /* Requisito 3 & 4
 - apertar no botao para gerar a carta
 - o texto deve permanecer no campo input
@@ -15,6 +23,12 @@ function addSpanToParagraph() {
   const generateButton = document.querySelector('button');
   const myParagraph = document.querySelector('#carta-gerada');
   generateButton.addEventListener('click', () => {
+    verifyInput();
+    while (myParagraph.childNodes.length > 0) {
+      myParagraph.childNodes.forEach((node) => {
+        myParagraph.removeChild(node);
+      });
+    }
     const wordsList = document.querySelector('input').value.split(' ');
     wordsList.forEach((word) => {
       const newSpan = createSpan(word);

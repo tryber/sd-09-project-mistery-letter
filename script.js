25ed048206f55f32de977f293d0714db76f88b06
Qu4k3r <neves0007@gmail.com>
2020-12-16 14:14:59 -0300

    Requisitos 3,4 & 5 atualizados

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-/* Requisito 5 - Se o usuário não preencher o campo ou preencher com
-apenas espaços vazios adicionar a mensagem:
-'Por favor, digite o conteúdo da carta.
-*/
-
-function verifyInput() {
-  if (document.querySelector('input').value.trim() === '') {
-    alert('Por favor, digite o conteúdo da carta.');
-    return;
-  }
-}
-
-/* Requisito 3 & 4
+/* Requisito 3, 4 & 5 (respectivamente)
 - apertar no botao para gerar a carta
-- o texto deve permanecer no campo input
 - cada palavra deve ser colocada em uma tag span
 - toda tag span deve ser adicionada como filha da tag p
+
+- o texto deve permanecer no campo input
+
+- Se o usuário não preencher o campo ou preencher com
+- apenas espaços vazios adicionar a mensagem:
+'Por favor, digite o conteúdo da carta.
 */
 
 function createSpan(text) {
@@ -27,7 +20,9 @@ function addSpanToParagraph() {
   const generateButton = document.querySelector('button');
   const myParagraph = document.querySelector('#carta-gerada');
   generateButton.addEventListener('click', () => {
-    verifyInput();
+    if (document.querySelector('#carta-texto').value.trim() === '') {
+      return (myParagraph.innerText = 'Por favor, digite o conteúdo da carta.')
+    }
     while (myParagraph.childNodes.length > 0) {
       myParagraph.childNodes.forEach((node) => {
         myParagraph.removeChild(node);

920f68c6c3ef0859c0ca61de2391a141457f2fd6
MZTBTG <mztbtg@hotmail.com>
2020-12-16 21:25:56 -0300

    corrigindo mais uma vez botão que adiciona o texto, mais uma vez...

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const buttonCreateText = document.getElementById('criar-carta');
 const textInput = document.getElementById('carta-texto');
 const textResult = document.getElementById('carta-gerada');
 
+function aleatoryClass() {
+  const classList = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+  let aleatory = Math.round(Math.random() * classList.length);
+  let classResult;
+  if (aleatory < 1) {
+    classResult = classList[0];
+  } else {
+    classResult = classList[aleatory - 1];
+  }
+  console.log(aleatory)
+  console.log(classResult)
+  return classResult;
+}
+
 function addText() {
   if (textInput.value || textInput.value !== ' ') {
     const word = textInput.value.split(' ');
     for (let index in word) {
       const newWord = document.createElement('span');
-      // newWord.className = 'teste';
+      newWord.className = aleatoryClass();
       newWord.innerText += word[index];
       textResult.appendChild(newWord);
     }

b8a8eb133d1427b323b9beed9b457de275840e23
MZTBTG <mztbtg@hotmail.com>
2020-12-17 13:56:55 -0300

    correções do codeclimate

--- script.js
+++ script.js
@@ -4,41 +4,37 @@ const textResult = document.getElementById('carta-gerada');
 
 function aleatoryClass() {
   const classList = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
-  let aleatory = Math.round(Math.random() * classList.length);
+  const aleatory = Math.round(Math.random() * classList.length);
   let classResult;
   if (aleatory < 1) {
     classResult = classList[0];
   } else {
     classResult = classList[aleatory - 1];
   }
-  console.log(aleatory)
-  console.log(classResult)
   return classResult;
 }
 
 function addText() {
   const word = textInput.value.split(' ');
   let problem = true;
-  console.log(typeof word);
-  console.log(word[0]);
-  for (let index in word) {
+  for (let index = 0; index < word.length; index += 1) {
     if (word[index] !== '') {
       problem = false;
     }
-    if (word[index] !== ' ' && typeof word === String) {
+    if (word[index] !== ' ' && typeof word == String) {
       problem = false;
     }
   }
   if (problem === false) {
     textResult.innerHTML = '';
-    for (let index in word) {
+    for (let index = 0; index < word.length; index += 1) {
       const newWord = document.createElement('span');
       newWord.className = aleatoryClass();
       newWord.innerText += word[index];
       textResult.appendChild(newWord);
     }
   } else {
-      textResult.innerHTML = 'por favor, digite o conteúdo da carta.';
+    textResult.innerHTML = 'por favor, digite o conteúdo da carta.';
   }
 }
 

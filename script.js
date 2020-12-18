096ef562df7735d4dc75cce20a94e20976c23a5a
rach-vp <picanco.raquel@gmail.com>
2020-12-17 22:28:49 -0400

    Trata erro da caixa vazia e cria classes

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function createEvent() {
   const createBtn = document.querySelector('#criar-carta');
   createBtn.addEventListener('click', function () {
     const inputText = document.querySelector('#carta-texto');
-    const printedLetter = document.querySelector('#carta-gerada');
-    const arrayText = inputText.value.split(' ');
-    for (let index = 0; index < arrayText.length; index += 1) {
-      const newSpan = document.createElement('span');
-      newSpan.innerText = arrayText[index];
-      printedLetter.appendChild(newSpan);
+    if (!inputText.value) {
+      alert('Por favor, digite o conteúdo da carta.');
+    } else {
+      const printedLetter = document.querySelector('#carta-gerada');
+      const arrayText = inputText.value.split(' ');
+      for (let index = 0; index < arrayText.length; index += 1) {
+        const newSpan = document.createElement('span');
+        newSpan.innerText = arrayText[index];
+        printedLetter.appendChild(newSpan);
+      }
     }
-
   });
 }
 

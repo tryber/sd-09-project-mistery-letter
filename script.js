81745ef466cd55224b7b3816938318d4d5237f9a
danielfasanaro <danielfasanaro@gmail.com>
2020-12-18 23:50:27 -0300

    3rd Requiriment - Added button #criar-carta and createLetter function

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const criarCartaButton = document.getElementById('criar-carta');
 const cartaGeradaParagraph = document.getElementById('carta-gerada');
 const cartaTextoInput = document.getElementById('carta-texto');
 
-function createLetter(){
-  const letter = cartaTextoInput.value.split(" ");
-  for (let word of letter) {
-    const wordSpan = document.createElement('span');
-    wordSpan.innerText = word;
-    cartaGeradaParagraph.appendChild(wordSpan);
+function createLetter() {
+  const letter = cartaTextoInput.value.split(' ');
+  if (letter) {
+    cartaGeradaParagraph.innerHTML = '';
+    console.log(letter);
+    for (let index = 0; index < letter.length; index += 1) {
+      const wordSpan = document.createElement('span');
+      wordSpan.innerText = letter[index];
+      cartaGeradaParagraph.appendChild(wordSpan);
+    }
   }
 }
 

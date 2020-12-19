3ae7076200003122231819700ff1cce37bd06f44
danielfasanaro <danielfasanaro@gmail.com>
2020-12-19 00:24:59 -0300

    5th Requiriment - Added error handler for empty letter

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const criarCartaButton = document.getElementById('criar-carta');
 const cartaGeradaParagraph = document.getElementById('carta-gerada');
 const cartaTextoInput = document.getElementById('carta-texto');
 
+function createWords(letter) {
+  for (let index = 0; index < letter.length; index += 1) {
+    if (letter[index]) {
+      const wordSpan = document.createElement('span');
+      wordSpan.innerText = letter[index];
+      cartaGeradaParagraph.appendChild(wordSpan);
+    }
+  }
+}
+
 function createLetter() {
   if (cartaTextoInput.value.trim()) {
     const letter = cartaTextoInput.value.trim().split(' ');
     cartaGeradaParagraph.innerHTML = '';
-    for (let index = 0; index < letter.length; index += 1) {
-      if (letter[index]) {
-        const wordSpan = document.createElement('span');
-        wordSpan.innerText = letter[index];
-        cartaGeradaParagraph.appendChild(wordSpan);
-      }
-    }
   } else {
-    alert('Por favor, digite o conteúdo da carta.');
+    cartaGeradaParagraph.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 

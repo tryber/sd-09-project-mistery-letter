9e82da96a62a00d228e64fd4a032a2d79aa2de22
Daniela Muller <danielamuller20@gmail.com>
2020-12-23 10:30:04 -0300

    Mensagem de erro

---
+++ script.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('#carta-texto');
+const button = document.querySelector('#criar-carta');
+const paragraph = document.querySelector('#carta-gerada');
+
+function errorMessage() {
+  button.addEventListener('click', function () {
+    const inputContent = input.value;
+    const spanMessage = document.createElement('span');
+    spanMessage.id = 'error-message'
+    if (inputContent.match(/^\s*$/)) { // Nada ou espaço vazio
+      spanMessage.innerHTML = 'Por favor, digite o conteúdo da carta.';
+      paragraph.appendChild(spanMessage);
+    } else {
+      const messageError = document.querySelector('#error-message')
+      messageError.parentNode.removeChild(messageError)
+    }
+  });
+}
+errorMessage();
+
+function getWord() {
+button.addEventListener('click', function () {
+  const wordString = (input.value);
+  const wordsArray = wordString.split(' ');
+  
+  for (let index = 0; index < wordsArray.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerHTML = wordsArray[index];
+    paragraph.appendChild(span);
+  }
+});
+}
+getWord();
+

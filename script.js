34bf7c9b35009a175bd1d026bbec3735f99b22b5
Daniela Muller <danielamuller20@gmail.com>
2020-12-23 10:45:29 -0300

    Classes

--- script.js
+++ script.js
@@ -5,30 +5,31 @@ const paragraph = document.querySelector('#carta-gerada');
 function errorMessage() {
   button.addEventListener('click', function () {
     const inputContent = input.value;
-    const spanMessage = document.createElement('span');
-    spanMessage.id = 'error-message'
+    //const spanMessage = document.createElement('span');
+    //spanMessage.id = 'error-message';
     if (inputContent.match(/^\s*$/)) { // Nada ou espaço vazio
-      spanMessage.innerHTML = 'Por favor, digite o conteúdo da carta.';
-      paragraph.appendChild(spanMessage);
+     alert('Por favor, digite o conteúdo da carta.');
+      // spanMessage.innerHTML = 'Por favor, digite o conteúdo da carta.';
+      // paragraph.appendChild(spanMessage);
     } else {
-      const messageError = document.querySelector('#error-message')
-      messageError.parentNode.removeChild(messageError)
+        getWord();
     }
+    //   const messageError = document.querySelector('#error-message');
+    //   messageError.parentNode.removeChild(messageError);
+    // }
   });
 }
 errorMessage();
 
 function getWord() {
-button.addEventListener('click', function () {
-  const wordString = (input.value);
-  const wordsArray = wordString.split(' ');
-  
-  for (let index = 0; index < wordsArray.length; index += 1) {
-    const span = document.createElement('span');
-    span.innerHTML = wordsArray[index];
-    paragraph.appendChild(span);
-  }
-});
+  button.addEventListener('click', function () {
+    const wordString = (input.value);
+    const wordsArray = wordString.split(' ');
+    for (let index = 0; index < wordsArray.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerHTML = wordsArray[index];
+      paragraph.appendChild(span);
+    }
+  });
 }
 getWord();
-

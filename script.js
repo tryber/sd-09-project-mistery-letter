238a601c8bd98c0f6cca411851f3c054fc1d3c4f
Daniela Muller <danielamuller20@gmail.com>
2020-12-23 15:02:18 -0300

    Mensagem de erro e criação de cartas

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const paragraph = document.querySelector('#carta-gerada');
 
 function errorMessage() {
   const spanMessage = document.createElement('span');
-  paragraph.appendChild(spanMessage);
-  spanMessage.id = 'error-message';
   button.addEventListener('click', function () {
     const inputContent = input.value;
+    paragraph.appendChild(spanMessage);
+    spanMessage.id = 'error-message';
     if (inputContent.match(/^\s*$/)) { // Nada ou espaço vazio
       spanMessage.innerHTML = 'Por favor, digite o conteúdo da carta.';
     } else {
-      spanMessage.innerHTML = '';
-      // const messageError = document.querySelector('#error-message');
-      // messageError.parentNode.removeChild(messageError);
+      const getSpan = document.querySelector('#error-message');
+      getSpan.parentNode.removeChild(getSpan);
     }
   });
 }
@@ -23,10 +22,14 @@ function getWord() {
   button.addEventListener('click', function () {
     const wordString = (input.value);
     if (!wordString.match(/^\s*$/)) {
+      while (paragraph.firstChild) {
+        paragraph.removeChild(paragraph.lastChild);
+      }
       const wordsArray = wordString.split(' ');
       for (let index = 0; index < wordsArray.length; index += 1) {
         const span = document.createElement('span');
         span.innerHTML = wordsArray[index];
+        span.class = 'text';
         paragraph.appendChild(span);
       }
     }

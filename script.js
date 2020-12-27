0e2c0188b4a45b3c00e8bf9f66fc032576ba1c15
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-27 12:43:46 -0300

    adicionando funcionalidades dinamicas

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const inputField = document.querySelector('#carta-texto');
-const inputButton = document.querySelector('#criar-carta');
 const textContainer = document.querySelector('#carta-gerada');
-const counter = document.querySelector('#carta-contador');
 
-inputButton.addEventListener('click', function () {
-  word = document.createElement('span');
-  word.innerHTML = inputField.value;
-  textContainer.appendChild(word);
-});
+function appendRandomClass() {
+  const styleClasses = ['newspaper', 'magazine1', 'magazine2'];
+  const sizeClasses = ['medium', 'big', 'reallybig'];
+  const rotateClasses = ['rotateleft', 'rotateright'];
+  const skewClasses = ['skewleft', 'skewright'];
+}
+
+function letterCounter() {
+  const wordCounter = document.querySelector('#carta-contador');
+  const letter = document.querySelectorAll('span');
+  wordCounter.innerText = `${letter.length} words`
+}
+
+function generateLetter() {
+  const phrase = inputField.value.split(' ');
+  for (index = 0; index < phrase.length; index += 1) {
+    const word = document.createElement('span');
+    // word.classList = appendRandomClass();
+    word.innerText = `${phrase[index]} `;
+    textContainer.appendChild(word);
+  }
+  letterCounter()
+}
+
+function checkInputValue() {
+  if (inputField.value.length > 0) {
+    generateLetter();
+  } else {
+    alert('Por favor, digite o conteúdo da carta.');
+  }
+}
+
+window.onload = function () {
+  const inputButton = document.querySelector('#criar-carta');
+  inputButton.addEventListener('click', checkInputValue);
+};

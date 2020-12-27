51d7818f31dbb37c4703fd19f64f5107ff58e364
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-27 18:39:27 -0300

    feito requisito 18

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function createLetter() {
   const text = document.querySelector('#carta-texto');
   const listWords = text.value.split(' ');
   const paragraph = document.querySelector('#carta-gerada');
+  const count = document.querySelector('#carta-contador');
+  paragraph.innerText = '';
   if (listWords[0] === '') {
     paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
+    count.innerText = '0 words';
   } else {
     for (let index = 0; index < listWords.length; index += 1) {
+      count.innerText = `${listWords.length} words`;
       const span = document.createElement('span');
       span.innerText = listWords[index];
       paragraph.appendChild(span);

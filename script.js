4d897d9ed03c3090b7651fd9575f527b096ac5bd
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-26 22:47:39 -0300

    corrigindo issues do codeclimate

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const input = document.querySelector('#text');
+const inputField = document.querySelector('#carta-texto');
 const inputButton = document.querySelector('#criar-carta');
 const textContainer = document.querySelector('#carta-gerada');
+const counter = document.querySelector('#carta-contador');
+
+inputButton.addEventListener('click', function () {
+  word = document.createElement('span');
+  word.innerHTML = inputField.value;
+  textContainer.appendChild(word);
+});

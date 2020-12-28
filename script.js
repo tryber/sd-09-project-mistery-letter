ad8d8df91c698e33969eac5e4c43011f1481aee9
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-27 21:19:54 -0300

    refatorando script.js

--- script.js
+++ script.js
@@ -1,48 +1,20 @@
-const input = document.querySelector('#carta-texto');
-const letterText = document.querySelector('#carta-gerada');
-const error = 'Por favor, digite o conteúdo da carta.';
-
-const styleClasses = [
-  ['newspaper', 'magazine1', 'magazine2'],
-  ['medium', 'big', 'reallybig'],
-  ['rotateleft', 'rotateright'],
-  ['skewleft', 'skewright']
-];
-
-function letterCounter() {
-  const wordCounter = document.querySelector('#carta-contador');
-  const letter = document.querySelectorAll('span');
-  wordCounter.innerText = letter.length;
-}
-
-function generateLetterText() {
-  const phrase = input.value.split(' ');
-  for (let index = 0; index < phrase.length; index += 1) {
-    const word = document.createElement('span');
-    word.innerText = phrase[index];
-    letterText.appendChild(word);
-    letterText.innerHTML += ' ';
-  }
-}
-
 function generateLetter() {
-  if (checkInputValue() === true) {
-    generateLetterText();
-    letterCounter();
+  const input = document.querySelector('#carta-texto');
+  const letterText = document.querySelector('#carta-gerada');
+  const counter = document.querySelector('#carta-contador');
+  const phrase = input.value.split(' ');
+  letterText.innerText = '';
+  if (input.value.trim() === '') {
+    letterText.innerText = 'Por favor, digite o conteúdo da carta.';
+    counter.innerText = 0;
   } else {
-    letterText.innerHTML = error;
+    for (let index = 0; index < phrase.length; index += 1) {
+      const word = document.createElement('span');
+      word.innerText = phrase[index];
+      letterText.appendChild(word);
+    }
+    counter.innerText = letterText.children.length;
   }
-}
-
-function checkInputValue() {
-  let check = false;
-  if (input.value.trim().length > 0 && letterText.innerText === error) {
-    letterText.innerText = '';
-    check = true;
-  } else if (input.value.trim().length > 0 && letterText.innerText !== error) {
-    check = true;
-  }
-  return check;
 }
 
 window.onload = function () {

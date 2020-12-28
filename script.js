c34f190aee1550329a934efc096573c3a0e7ac6a
LucioOSilva <lucioosilva00@gmail.com>
2020-12-28 20:57:27 -0300

    adjusts on create letter button2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function submitLetter() {
   const button = document.querySelector('#criar-carta');
-  const formText = document.querySelector('#form-texto');
-  const paragraph = document.querySelector('p');
-  button.addEventListener('click', function (event) {
-    event.preventDefault();
-    const array = formText.children[0].value.split(' ');
+  const inputText = document.querySelector('#carta-texto');
+  const paragraph = document.querySelector('#carta-gerada');
+  button.addEventListener('click', function () {
+    const array = inputText.value.split(' ');
+    paragraph.innerText = '';
     for (let index = 0; index < array.length; index += 1) {
       const spanElement = document.createElement('span');
       spanElement.innerText = array[index];
       paragraph.appendChild(spanElement);
+      console.log(index)
     }
   });
 }

add3c13037857d189881a29131c983b44c94d22d
Natalia Negreiros <nataliaknegres@gmail.com>
2020-12-28 15:48:44 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const paragraph = document.querySelector('#carta-gerada');
 button.addEventListener('click', () => {
   const word = input.value;
   if (word.length === 0) {
-    const message = document.createElement('p');
-    message.className = 'messageError';
-    message.innerText = 'Por favor, digite o conteúdo da carta.';
-    paragraph.appendChild(message);
+    paragraph.innerHTML = 'Por favor, digite o conteúdo da carta.'
   } else {
     const newWords = word.split(' ');
     for (let index = 0; index < newWords.length; index += 1) {

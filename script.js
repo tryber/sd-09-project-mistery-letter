bfda83339b5e17ac7ee9209fa7229e3deb4d4d4d
LucioOSilva <lucioosilva00@gmail.com>
2020-12-28 20:46:05 -0300

    adjusts on create letter button

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ function submitLetter() {
   const paragraph = document.querySelector('p');
   button.addEventListener('click', function (event) {
     event.preventDefault();
-    let array = formText.children[0].value.split(' ');
+    const array = formText.children[0].value.split(' ');
     for (let index = 0; index < array.length; index += 1) {
       const spanElement = document.createElement('span');
       spanElement.innerText = array[index];
-      console.log(spanElement);
       paragraph.appendChild(spanElement);
     }
   });

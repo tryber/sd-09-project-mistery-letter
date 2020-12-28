331b597ae9d53058ab9a613a896266059967d516
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-27 22:54:29 -0300

    requisito 16 feito

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const input = document.querySelector('#carta-texto');
+const letterText = document.querySelector('#carta-gerada');
+const counter = document.querySelector('#carta-contador');
+
+const styleClass = ['newspaper', 'magazine1', 'magazine2'];
+const sizeClass = ['medium', 'big', 'reallybig'];
+const rotateClass = ['rotateleft', 'rotateright'];
+const skewClass = ['skewleft', 'skewright'];
+
+function generateRandomClass(event, className) {
+  const randomClass = className[Math.floor(Math.random() * className.length)];
+  event.classList.add(randomClass);
+}
+
 function generateLetter() {
-  const input = document.querySelector('#carta-texto');
-  const letterText = document.querySelector('#carta-gerada');
-  const counter = document.querySelector('#carta-contador');
   const phrase = input.value.split(' ');
   letterText.innerText = '';
   if (input.value.trim() === '') {
@@ -12,6 +23,11 @@ function generateLetter() {
       const word = document.createElement('span');
       word.innerText = phrase[index];
       letterText.appendChild(word);
+
+      generateRandomClass(word, styleClass);
+      generateRandomClass(word, sizeClass);
+      generateRandomClass(word, rotateClass);
+      generateRandomClass(word, skewClass);
     }
     counter.innerText = letterText.children.length;
   }

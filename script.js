4f1f8d359949aaf51126f0e96e7921de40753fa8
LucioOSilva <lucioosilva00@gmail.com>
2020-12-28 21:47:11 -0300

    added verify empty spaces and added classes

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-function submitLetter() {
-  const button = document.querySelector('#criar-carta');
-  const inputText = document.querySelector('#carta-texto');
+function submitLetter(phrase) {
   const paragraph = document.querySelector('#carta-gerada');
+  const array = phrase.split(' ');
+  paragraph.innerText = '';
+  for (let index = 0; index < array.length; index += 1) {
+    const spanElement = document.createElement('span');
+    spanElement.innerText = array[index];
+    paragraph.appendChild(spanElement);
+  }
+}
+
+function verifyPhrase() {
+  const inputText = document.querySelector('#carta-texto');
+  const button = document.querySelector('#criar-carta');
   button.addEventListener('click', function () {
-    const array = inputText.value.split(' ');
-    paragraph.innerText = '';
-    for (let index = 0; index < array.length; index += 1) {
-      const spanElement = document.createElement('span');
-      spanElement.innerText = array[index];
-      paragraph.appendChild(spanElement);
-      console.log(index)
+    if(inputText.value.trim()){
+      submitLetter(inputText.value);
+    } else {
+      submitLetter('Por favor, digite o conteúdo da carta.')
     }
   });
 }
 
-submitLetter();
+verifyPhrase();

ccb19e709913436a8739d57c50990652c2bcb2f7
Osvaldo Borges <netoo70@gmail.com>
2020-12-29 19:38:56 -0300

    Requisito 17

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const styleClass = ['magazine1', 'magazine2', 'newspaper'];
+const sizesClass = ['medium', 'big', 'reallybig'];
+const rotationsClass =['rotateleft', 'rotateright'];
+const skewClass = ['skewleft', 'skewright'];
+
 function removeMensage() {
   const span = document.querySelectorAll('span');
   for (let index = 0; index < span.length; index += 1) {
@@ -6,10 +11,28 @@ function removeMensage() {
   }
 }
 
+function randomClass(mensage, arrayClass) {
+  const classN = arrayClass[Math.floor(Math.random() * arrayClass.length)];
+  mensage.className += `${classN} `;
+}
+
+function createSpan(mensageClear) {
+  const father = document.querySelector('#carta-gerada');
+  const mensageSpan = document.createElement('span');
+  mensageSpan.innerText = `${mensageClear}`;
+  randomClass(mensageSpan, styleClass);
+  randomClass(mensageSpan, sizesClass);
+  randomClass(mensageSpan, rotationsClass);
+  randomClass(mensageSpan, skewClass);
+  father.appendChild(mensageSpan);
+}
+
 function createMensage() {
   const father = document.querySelector('#carta-gerada');
   const inputMensage = document.querySelector('#carta-texto');
+  const counter = document.querySelector('#carta-contador')
   let mensage = inputMensage.value.trim();
+  counter.innerText = 0;
   let mensageClear = '';
   father.innerText = '';
   removeMensage();
@@ -18,16 +41,13 @@ function createMensage() {
       if (mensage[index] !== ' ') {
         mensageClear += mensage[index];
       } else {
-        const mensageSpan = document.createElement('span');
-        mensageSpan.innerText = `${mensageClear}`;
-        father.appendChild(mensageSpan);
+        createSpan(mensageClear)
         mensageClear = '';
-
+        
       }
     }
-    const mensageSpan = document.createElement('span');
-    mensageSpan.innerText = `${mensageClear}`;
-    father.appendChild(mensageSpan);
+    createSpan(mensageClear);
+    counter.innerText = father.childElementCount;
   } else {
     father.innerText = 'Por favor, digite o conteúdo da carta.';
   }

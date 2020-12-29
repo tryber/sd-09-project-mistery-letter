30d3c39c12bf839a31c616a714b2e372d2d5ecd0
LucioOSilva <lucioosilva00@gmail.com>
2020-12-28 23:01:11 -0300

    code climate indentation adjusts1

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 function addClassText(element) {
-  const styles = {
-    1: 'newspaper',
-    2: 'magazine1',
-    3: 'magazine2'
-  }
-  const sizes = {
-    1: 'medium',
-    2: 'big',
-    3: 'reallybig'
-  }
-  const rotates = {
-    1: 'rotateleft',
-    2: 'rotateright',
-    3: 'rotatenone'
-  }
   const inclinations = {
     1: 'skewleft',
     2: 'skewright',
-    3: 'skewnone'
-  }
-  randomInclinations = Math.ceil(Math.random()*3);
-  randomRotates = Math.ceil(Math.random()*3);
-  randomStyles = Math.ceil(Math.random()*3);
-  randomSizes = Math.ceil(Math.random()*3);
+  };
+  const rotates = {
+    1: 'rotateleft',
+    2: 'rotateright',
+  };
+  const styles = {
+    1: 'newspaper',
+    2: 'magazine1',
+    3: 'magazine2',
+  };
+  const sizes = {
+    1: 'medium',
+    2: 'big',
+    3: 'reallybig',
+  };
+  const randomInclinations = Math.ceil(Math.random() * 2);
+  const randomRotates = Math.ceil(Math.random() * 2);
+  const randomStyles = Math.ceil(Math.random() * 3);
+  const randomSizes = Math.ceil(Math.random() * 3);
   element.className = `${inclinations[randomInclinations]} ${rotates[randomRotates]} ${styles[randomStyles]} ${sizes[randomSizes]}`;
 }
 
 function submitLetter(phrase) {
   const paragraph = document.querySelector('#carta-gerada');
+  const counter = document.querySelector('#carta-contador');
   const array = phrase.split(' ');
+  counter.innerText = array.length;
   paragraph.innerText = '';
   for (let index = 0; index < array.length; index += 1) {
     const spanElement = document.createElement('span');
@@ -41,11 +41,12 @@ function submitLetter(phrase) {
 function verifyPhrase() {
   const inputText = document.querySelector('#carta-texto');
   const button = document.querySelector('#criar-carta');
+  const paragraph = document.querySelector('#carta-gerada');
   button.addEventListener('click', function () {
-    if(inputText.value.trim()){
+    if (inputText.value.trim()) {
       submitLetter(inputText.value);
     } else {
-      submitLetter('Por favor, digite o conteúdo da carta.')
+      paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
     }
   });
 }

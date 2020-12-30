7c74b6f351fbd29e74d873437f02618e0b49b40c
Osvaldo Borges <netoo70@gmail.com>
2020-12-29 21:59:48 -0300

    CodeClimate

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const father = document.querySelector('#carta-gerada');
+const inputMensage = document.querySelector('#carta-texto');
+const counter = document.querySelector('#carta-contador')
+const buttonCreate = document.querySelector('#criar-carta');
+
 const styleClass = ['magazine1', 'magazine2', 'newspaper'];
 const sizesClass = ['medium', 'big', 'reallybig'];
 const rotationsClass = ['rotateleft', 'rotateright'];
@@ -12,65 +17,38 @@ function removeMensage() {
   }
 }
 
-//Referencia: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Gerando_um_n%C3%BAmero_inteiro_aleat%C3%B3rio_entre_dois_valores
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-console.log(groups.length)
-
-function randomWord(origin) {
-  const word = origin.target;
-  randomClass(word, styleClass);
-}
-
 function randomClass(mensage, arrayClass) {
-  getRandomInt(2, groups);
-
   const classN = arrayClass[Math.floor(Math.random() * arrayClass.length)];
   mensage.classList.add(classN);
 }
 
 function createSpan(mensageClear) {
-  const father = document.querySelector('#carta-gerada');
   const mensageSpan = document.createElement('span');
   mensageSpan.innerText = `${mensageClear}`;
-  mensageSpan.addEventListener('click', randomWord)
   father.appendChild(mensageSpan);
+
   randomClass(mensageSpan, styleClass);
   randomClass(mensageSpan, sizesClass);
   randomClass(mensageSpan, rotationsClass);
   randomClass(mensageSpan, skewClass);
 }
 
+//Referencia: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+//Referencia: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
+
 function createMensage() {
-  const father = document.querySelector('#carta-gerada');
-  const inputMensage = document.querySelector('#carta-texto');
-  const counter = document.querySelector('#carta-contador')
-  let mensage = inputMensage.value.trim();
+  const mensage = inputMensage.value.split(' ');
   counter.innerText = 0;
-  let mensageClear = '';
   father.innerText = '';
   removeMensage();
-  if (mensage !== '') {
+  if (inputMensage.value.trim() !== '') {
     for (let index = 0; index < mensage.length; index += 1) {
-      if (mensage[index] !== ' ') {
-        mensageClear += mensage[index];
-      } else {
-        createSpan(mensageClear)
-        mensageClear = '';
-        
-      }
+      createSpan(mensage[index]);
     }
-    createSpan(mensageClear);
     counter.innerText = father.childElementCount;
   } else {
     father.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
-const buttonCreate = document.querySelector('#criar-carta');
 buttonCreate.addEventListener('click', createMensage);

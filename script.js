222f8bb63f011533b8c1f84d9f9501556995c95d
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-31 13:58:09 -0400

    Adiciona requisitos 1 ao 3

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#carta-texto');
+const button = document.querySelector('#criar-carta');
+const letter = document.querySelector('#carta-gerada');
+let splitInput = '';
+
+function createLetter() {
+  splitInput = input.value.split(' ');
+  for (let index = 0; index < splitInput.length; index += 1) {
+    const spanCreate = document.createElement('span');
+    spanCreate.innerText = splitInput[index];
+    letter.appendChild(spanCreate);
+  }
+}
+
+function testInput() {
+  if (input.value === '' || input.value[0] === ' ') {
+    window.alert('Por favor, digite o conteúdo da carta.');
+  }
+}
+
+function buttonCreate() {
+  button.addEventListener('click', testInput);
+}
+
+buttonCreate();

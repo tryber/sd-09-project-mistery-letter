c3f95920af0fa6acfc59909fd69ae47ba517cc0e
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-03 16:46:24 -0400

    Adiciona ultimo requisito

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const input = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
 const letter = document.querySelector('#carta-gerada');
+const contador = document.querySelector('#carta-contador');
 let splitInput = '';
 const classes = {
   style: ['newspaper', 'magazine1', 'magazine2'],
   size: ['medium', 'big', 'reallybig'],
   rotate: ['rotateleft', 'rotateright'],
-  inclined: ['skewleft', 'skewright']
-}
+  inclined: ['skewleft', 'skewright'],
+};
 
 function removeLetter() {
   const spans = letter.children.length;
@@ -20,14 +21,15 @@ function removeLetter() {
 }
 
 function randomize(spanCreate) {
-  spanCreate.classList.add(classes.style[Math.round(Math.random() * 2)])
-  spanCreate.classList.add(classes.size[Math.round(Math.random() * 2)])
-  spanCreate.classList.add(classes.rotate[Math.round(Math.random() * 1)])
-  spanCreate.classList.add( classes.inclined[Math.round(Math.random() * 1)])
+  spanCreate.classList.add(classes.style[Math.round(Math.random() * 2)]);
+  spanCreate.classList.add(classes.size[Math.round(Math.random() * 2)]);
+  spanCreate.classList.add(classes.rotate[Math.round(Math.random() * 1)]);
+  spanCreate.classList.add(classes.inclined[Math.round(Math.random() * 1)]);
   letter.appendChild(spanCreate);
 }
 
 function createLetter() {
+  let cont = 0;
   removeLetter();
   splitInput = input.value.split(' ');
   for (let index = 0; index < splitInput.length; index += 1) {
@@ -35,8 +37,10 @@ function createLetter() {
       const spanCreate = document.createElement('span');
       spanCreate.innerText = splitInput[index];
       randomize(spanCreate);
+      cont += 1;
     }
   }
+  contador.innerText = cont;
 }
 
 function testInput() {

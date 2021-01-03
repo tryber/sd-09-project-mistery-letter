0c8fc1935b59f2d682d3f800ce67044642180ff7
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-03 16:31:02 -0400

    Adiciona requisito 16

--- script.js
+++ script.js
@@ -2,20 +2,40 @@ const input = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
 const letter = document.querySelector('#carta-gerada');
 let splitInput = '';
+const classes = {
+  style: ['newspaper', 'magazine1', 'magazine2'],
+  size: ['medium', 'big', 'reallybig'],
+  rotate: ['rotateleft', 'rotateright'],
+  inclined: ['skewleft', 'skewright']
+}
 
-function createLetter() {
+function removeLetter() {
   const spans = letter.children.length;
   if (spans > 0) {
     for (let index = 0; index < spans; index += 1) {
       const child = letter.children[0];
-      letter.removeChild(child)
+      letter.removeChild(child);
     }
   }
+}
+
+function randomize(spanCreate) {
+  spanCreate.classList.add(classes.style[Math.round(Math.random() * 2)])
+  spanCreate.classList.add(classes.size[Math.round(Math.random() * 2)])
+  spanCreate.classList.add(classes.rotate[Math.round(Math.random() * 1)])
+  spanCreate.classList.add( classes.inclined[Math.round(Math.random() * 1)])
+  letter.appendChild(spanCreate);
+}
+
+function createLetter() {
+  removeLetter();
   splitInput = input.value.split(' ');
   for (let index = 0; index < splitInput.length; index += 1) {
-    const spanCreate = document.createElement('span');
-    spanCreate.innerText = splitInput[index];
-    letter.appendChild(spanCreate);
+    if (splitInput[index] !== '') {
+      const spanCreate = document.createElement('span');
+      spanCreate.innerText = splitInput[index];
+      randomize(spanCreate);
+    }
   }
 }
 

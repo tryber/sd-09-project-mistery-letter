7b0be425bb674fe6ef63d72be99e9674e8a92b85
Enio Nicael <enionicael@gmail.com>
2021-01-04 15:54:50 -0300

    Feature: verifica se foi digitado algo ou se foi digitado somente espacos vazios.

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const splitText = function (array) {
-  const letter = array.split(' ');
+const validateInput = function (text) {
+  const finalText = text.trim();
+  if (finalText.length == 0) {
+    return false;
+  }
+  return true;
+};
+
+const splitText = function (text) {
+  const letter = text.split(' ');
   return letter;
 };
 
 function handleClickButton() {
   const inputText = document.getElementById('carta-texto').value;
-  const letter = splitText(inputText);
+  console.log(inputText);
   const pElement = document.getElementById('carta-gerada');
-
-  for (let index = 0; index < letter.length; index += 1) {
+  const validInput = validateInput(inputText);
+  console.log(validInput);
+  if (validInput) {
+    const letter = splitText(inputText);
+    for (let index = 0; index < letter.length; index += 1) {
+      const spanElement = document.createElement('span');
+      spanElement.className = 'word';
+      spanElement.innerText = letter[index];
+      pElement.appendChild(spanElement);
+    }
+  } else {
     const spanElement = document.createElement('span');
-    spanElement.className = 'word';
-    spanElement.innerText = letter[index];
-    pElement.appendChild(spanElement);
+    spanElement.innerText = 'Por favor, digite o conteúdo da carta.';
   }
-
-  // console.log(pElement);
 }
 
 const createLetter = function () {
   const buttonElement = document.getElementById('criar-carta');
   buttonElement.addEventListener('click', handleClickButton);
-
-  // console.log(buttonElement);
 };
 
 window.onload = function () {
   createLetter();
-}
+};

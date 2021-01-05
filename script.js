57e3a9a0a29e553089d416e10efe2cb4691fd722
Kevin Fraga <kevinciolac@gmail.com>
2021-01-05 14:17:57 -0300

    :zero::five:

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const letter = document.getElementById('carta-gerada');
 
 function createLetter() {
   const text = message.value.split(' ');
+  const helper = message.value.replace(/\s/g, '');
+  if (text === '' || helper === '') {
+    message.value = 'Por favor, digite o conteúdo da carta';
+    return;
+  }
   for (let index = 0; index < text.length; index += 1) {
     const span = document.createElement('span');
     span.innerText = text[index];

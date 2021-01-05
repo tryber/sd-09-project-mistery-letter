5d00ee71122976e4dc390704aee33ba93e9d787e
Kevin Fraga <kevinciolac@gmail.com>
2021-01-05 15:18:30 -0300

    :zero::three: fix

--- script.js
+++ script.js
@@ -11,14 +11,15 @@ function destroyLetter() {
 
 function createLetter() {
   const text = message.value.replace(/\s/g, '');
+  const helper = message.value.split(' ');
   destroyLetter();
   if (text === '') {
     letter.innerText = 'Por favor, digite o conteúdo da carta.';
     return;
   }
-  for (let index = 0; index < text.length; index += 1) {
+  for (let index = 0; index < helper.length; index += 1) {
     const span = document.createElement('span');
-    span.innerText = text[index];
+    span.innerText = helper[index];
     letter.appendChild(span);
   }
 }

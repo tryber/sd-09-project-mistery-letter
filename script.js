35cb3a2058383493ec2bfa0cdd12c1b0286e4524
Nathália <nathaliaveneziano@icloud.com>
2021-01-06 10:15:28 -0300

    Correçao Evaluator 1

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function ramdomValue(maxNumber) {
 
 // Gerar classes
 function ramdomClasses() {
-  const grupoEstilo = ['', 'newspaper ', 'magazine1 ', 'magazine2 '];
-  const grupoTamanho = ['', 'medium ', 'big ', 'reallybig '];
-  const grupoRotacao = ['', 'rotateleft ', 'rotateright '];
-  const grupoInclinacao = ['', 'skewleft', 'skewright'];
+  const grupoEstilo = ['newspaper ', 'magazine1 ', 'magazine2 '];
+  const grupoTamanho = ['medium ', 'big ', 'reallybig '];
+  const grupoRotacao = ['rotateleft ', 'rotateright '];
+  const grupoInclinacao = ['skewleft', 'skewright'];
 
   let classes = '';
 
@@ -22,13 +22,11 @@ function ramdomClasses() {
 
 // Adiciona ao HTML Quantidade de Letras
 function includeQdtWords(length) {
-  const qtdWords = `Quantidade de Palvras: ${length}`;
-
   if (!document.getElementById('carta-contador')) {
     const pCartaGerada = document.getElementById('carta-gerada');
-    pCartaGerada.insertAdjacentHTML('afterend', `<p id="carta-contador">${qtdWords}</p>`);
+    pCartaGerada.insertAdjacentHTML('afterend', `<p id="carta-contador">${length}</p>`);
   } else {
-    document.getElementById('carta-contador').innerText = qtdWords;
+    document.getElementById('carta-contador').innerText = length;
   }
 }
 
@@ -50,7 +48,7 @@ document.querySelector('#criar-carta').addEventListener('click', function () {
     document.querySelector('#carta-texto').value = cartaTexto;
     includeQdtWords(cartaTextoLength);
   } else {
-    alert('Por favor, digite o conteúdo da carta.');
+    alert('por favor, digite o conteúdo da carta.');
   }
 });
 

74ecae6055110426b12b5722158bcedb7d6a2114
brenno-calado <brenno.calado@gmail.com>
2021-01-07 21:39:29 -0300

    create span with buildMisteryLetter

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function addEventListeners() {
-  const criarCarta = document.querySelector('#criar-carta');
-  criarCarta.addEventListener('click', createLetter);
+function buildMisteryLetter(texto) {
+  const cartaGerada = document.querySelector('#carta-gerada');
+  const splitText = texto.split(' ');
+  for (let index = 0; index < splitText.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerText = splitText[index];
+    cartaGerada.appendChild(span);
+  }
 }
 
-function createLetter() {
+function checkEmptyInput(texto) {
+  if(texto === ' ' || '') {
+    console.log(texto);
+    const cartaGerada = document.querySelector('#carta-gerada');
+    cartaGerada.innerText = 'por favor, digite o conteúdo da carta';
+  } else {
+    buildMisteryLetter(texto);
+  }
+}
+
+function receiveInput() {
   const cartaTexto = document.querySelector('#carta-texto');
   const texto = cartaTexto.value;
+  texto.replace(/\s\s+/g, ' ');
+  checkEmptyInput(texto);
+}
+
+function addEventListeners() {
+  const criarCarta = document.querySelector('#criar-carta');
+  criarCarta.addEventListener('click', receiveInput);
 }
 
 window.onload = function () {

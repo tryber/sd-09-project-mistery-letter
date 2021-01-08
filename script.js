be5c827f39ae74a470eaecca6e16b04d0adf1b1a
brenno-calado <brenno.calado@gmail.com>
2021-01-07 22:02:32 -0300

    add comments to plan buildSpanClasses(span)

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// no mínimo 2 classes de 2 grupos e no máximo 4 classes de 4 grupos
+// um número aleatório integral de 2 a 4 para escolher a quantidade de classes
+// para escolher o grupo de classe um número aleatório de 1 a 4, depois remove o índice da classe selecionada
+// para escolher a classe dentro do grupo um número aleatório que vai até o número de classes existentes
+
+function buildSpanClasses(span) {
+  
+}
+
 function buildMisteryLetter(texto) {
   const cartaGerada = document.querySelector('#carta-gerada');
   const splitText = texto.split(' ');
   for (let index = 0; index < splitText.length; index += 1) {
     const span = document.createElement('span');
+    buildSpanClasses(span);
     span.innerText = splitText[index];
     cartaGerada.appendChild(span);
   }

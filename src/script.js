c71390d53e586845952eed5ce6b53d08b7521b19
Matheus Martino <matheus.martino97@gmail.com>
2021-02-18 16:13:02 -0300

    Creating the css classes.

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const styleCSSClasses = [
+  'none',
+  'newspaper',
+  'magazine1',
+  'magazine2',
+]
+
+const sizeCSSClasses = [
+  'none',
+  'medium',
+  'big',
+  'reallybig',
+]
+
+const rotationCSSClasses = [
+  'none',
+  'rotateleft',
+  'rotateright',
+]
+
+const inclinationCSSClasses = [
+  'none',
+  'skewleft',
+  'skewright',
+]
